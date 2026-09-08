09f94f7e4815c4f43d12a19cfdd00bd85c6ce5a5
Андрій <andrulik1303@gmail.com>
2026-09-08 22:49:18 +0300

    Додано логіку експорту телеметрії в CSV

--- exportService.js
+++ exportService.js
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as Sharing from 'expo-sharing';
 import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 
